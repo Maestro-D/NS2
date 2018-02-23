d7e60607f29b66ee110215faee1f8799419bae56
Maestro-D <stend.kant@gmail.com>
2018-02-23 14:23:11 -0800

    exemple lib ssh

---
+++ sample_ssh_server/build/CMakeFiles/sample_sshd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_sshd.dir/samplesshd-kbdint.c.o"
+  "sample_sshd.pdb"
+  "sample_sshd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sample_sshd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sample_ssh_server/build/config.h
@@ -0,0 +1,204 @@
+/* Name of package */
+#define PACKAGE "sample_ssh_server"
+
+/* Version number of package */
+/* #undef VERSION */
+
+/* #undef LOCALEDIR */
+#define DATADIR "/usr/local/share/sample_ssh_server"
+#define LIBDIR "/usr/local/lib"
+#define PLUGINDIR "/usr/local/lib/sample_ssh_server-"
+#define SYSCONFDIR "/usr/local/etc"
+#define BINARYDIR "/home/qbayle/Test_ssh/spatch/libssh_examples/sample_ssh_server/build"
+#define SOURCEDIR "/home/qbayle/Test_ssh/spatch/libssh_examples/sample_ssh_server"
+
+/************************** HEADER FILES *************************/
+
+/* Define to 1 if you have the <argp.h> header file. */
+#define HAVE_ARGP_H 1
+
+/* Define to 1 if you have the <aprpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the <pty.h> header file. */
+#define HAVE_PTY_H 1
+
+/* Define to 1 if you have the <utmp.h> header file. */
+#define HAVE_UTMP_H 1
+
+/* Define to 1 if you have the <util.h> header file. */
+/* #undef HAVE_UTIL_H */
+
+/* Define to 1 if you have the <libutil.h> header file. */
+/* #undef HAVE_LIBUTIL_H */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/utime.h> header file. */
+/* #undef HAVE_SYS_UTIME_H */
+
+/* Define to 1 if you have the <io.h> header file. */
+/* #undef HAVE_IO_H */
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <openssl/aes.h> header file. */
+/* #undef HAVE_OPENSSL_AES_H */
+
+/* Define to 1 if you have the <wspiapi.h> header file. */
+/* #undef HAVE_WSPIAPI_H */
+
+/* Define to 1 if you have the <openssl/blowfish.h> header file. */
+/* #undef HAVE_OPENSSL_BLOWFISH_H */
+
+/* Define to 1 if you have the <openssl/des.h> header file. */
+/* #undef HAVE_OPENSSL_DES_H */
+
+/* Define to 1 if you have the <openssl/ecdh.h> header file. */
+/* #undef HAVE_OPENSSL_ECDH_H */
+
+/* Define to 1 if you have the <openssl/ec.h> header file. */
+/* #undef HAVE_OPENSSL_EC_H */
+
+/* Define to 1 if you have the <openssl/ecdsa.h> header file. */
+/* #undef HAVE_OPENSSL_ECDSA_H */
+
+/* Define to 1 if you have the <pthread.h> header file. */
+/* #undef HAVE_PTHREAD_H */
+
+/* Define to 1 if you have eliptic curve cryptography in openssl */
+/* #undef HAVE_OPENSSL_ECC */
+
+/* Define to 1 if you have eliptic curve cryptography in gcrypt */
+/* #undef HAVE_GCRYPT_ECC */
+
+/* Define to 1 if you have eliptic curve cryptography */
+/* #undef HAVE_ECC */
+
+/*************************** FUNCTIONS ***************************/
+
+/* Define to 1 if you have the `EVP_aes128_ctr' function. */
+/* #undef HAVE_OPENSSL_EVP_AES_CTR */
+
+/* Define to 1 if you have the `EVP_aes128_cbc' function. */
+/* #undef HAVE_OPENSSL_EVP_AES_CBC */
+
+/* Define to 1 if you have the `CRYPTO_THREADID_set_callback' function. */
+/* #undef HAVE_OPENSSL_CRYPTO_THREADID_SET_CALLBACK */
+
+/* Define to 1 if you have the `CRYPTO_ctr128_encrypt' function. */
+/* #undef HAVE_OPENSSL_CRYPTO_CTR128_ENCRYPT */
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the `_snprintf' function. */
+/* #undef HAVE__SNPRINTF */
+
+/* Define to 1 if you have the `_snprintf_s' function. */
+/* #undef HAVE__SNPRINTF_S */
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if you have the `_vsnprintf' function. */
+/* #undef HAVE__VSNPRINTF */
+
+/* Define to 1 if you have the `_vsnprintf_s' function. */
+/* #undef HAVE__VSNPRINTF_S */
+
+/* Define to 1 if you have the `isblank' function. */
+#define HAVE_ISBLANK 1
+
+/* Define to 1 if you have the `strncpy' function. */
+#define HAVE_STRNCPY 1
+
+/* Define to 1 if you have the `cfmakeraw' function. */
+#define HAVE_CFMAKERAW 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
+#define HAVE_GETADDRINFO 1
+
+/* Define to 1 if you have the `poll' function. */
+#define HAVE_POLL 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `clock_gettime' function. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the `ntohll' function. */
+/* #undef HAVE_NTOHLL */
+
+/* Define to 1 if you have the `htonll' function. */
+/* #undef HAVE_HTONLL */
+
+/* Define to 1 if you have the `strtoull' function. */
+#define HAVE_STRTOULL 1
+
+/* Define to 1 if you have the `__strtoull' function. */
+/* #undef HAVE___STRTOULL */
+
+/* Define to 1 if you have the `_strtoui64' function. */
+/* #undef HAVE__STRTOUI64 */
+
+/*************************** LIBRARIES ***************************/
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+/* #undef HAVE_LIBCRYPTO */
+
+/* Define to 1 if you have the `gcrypt' library (-lgcrypt). */
+/* #undef HAVE_LIBGCRYPT */
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+/* #undef HAVE_PTHREAD */
+
+/**************************** OPTIONS ****************************/
+
+#define HAVE_GCC_THREAD_LOCAL_STORAGE 1
+/* #undef HAVE_MSC_THREAD_LOCAL_STORAGE */
+
+#define HAVE_GCC_VOLATILE_MEMORY_PROTECTION 1
+#define HAVE_GCC_NARG_MACRO 1
+
+#define HAVE_COMPILER__FUNC__ 1
+/* #undef HAVE_COMPILER__FUNCTION__ */
+
+/* Define to 1 if you want to enable GSSAPI */
+/* #undef WITH_GSSAPI */
+
+/* Define to 1 if you want to enable ZLIB */
+#define WITH_ZLIB 1
+
+/* Define to 1 if you want to enable SFTP */
+#define WITH_SFTP 1
+
+/* Define to 1 if you want to enable SSH1 */
+/* #undef WITH_SSH1 */
+
+/* Define to 1 if you want to enable server support */
+#define WITH_SERVER 1
+
+/* Define to 1 if you want to enable debug output for crypto functions */
+/* #undef DEBUG_CRYPTO */
+
+/* Define to 1 if you want to enable pcap output support (experimental) */
+#define WITH_PCAP 1
+
+/* Define to 1 if you want to enable calltrace debug output */
+#define DEBUG_CALLTRACE 1
+
+/* Define to 1 if you want to enable NaCl support */
+#define WITH_NACL 1
+
+/*************************** ENDIAN *****************************/
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+/* #undef WORDS_BIGENDIAN */
