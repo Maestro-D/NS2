25c713fa6b1adf1eaf05ff6ed887f48bbfa8b7db
Maestro-D <stend.kant@gmail.com>
2018-02-24 00:26:33 -0800

    Forwarding ok

--- src/cli_ssh.cpp
+++ src/cli_ssh.cpp
@@ -4,54 +4,6 @@
 #include <stdio.h>
 #include <libssh/libssh.h>
 
-int             forwarding_client(char* user, char* passwd, char* endpoint, int port)
-{
-  ssh_session   my_ssh_session;
-  int rc;
-
-  // Open session and set options
-  my_ssh_session = ssh_new();
-  if (my_ssh_session == NULL)
-    exit(-1);
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, endpoint);
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_PORT, port);
-
-  // Connect to server
-  rc = ssh_connect(my_ssh_session);
-  if (rc != SSH_OK)
-  {
-    fprintf(stderr, "Error connecting to localhost: %s\n",
-            ssh_get_error(my_ssh_session));
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
-
-  // Verify the server's identity
-  // For the source code of verify_knowhost(), check previous example
-  if (verify_knownhost(my_ssh_session) < 0)
-  {
-    ssh_disconnect(my_ssh_session);
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
-
-  // Authenticate ourselves
-  rc = ssh_userauth_password(my_ssh_session, user, passwd);
-
-  if (rc != SSH_AUTH_SUCCESS)
-  {
-    fprintf(stderr, "Error authenticating with password: %s\n",
-            ssh_get_error(my_ssh_session));
-    ssh_disconnect(my_ssh_session);
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
-
-  show_remote_processes(my_ssh_session);
-  ssh_disconnect(my_ssh_session);
-  ssh_free(my_ssh_session);
-}
-
 int verify_knownhost(ssh_session session)
 {
   int state, hlen;
@@ -151,7 +103,7 @@ int show_remote_processes(ssh_session session)
     }
     nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
   }
-    
+
   if (nbytes < 0)
   {
     ssh_channel_close(channel);
@@ -162,4 +114,52 @@ int show_remote_processes(ssh_session session)
   ssh_channel_close(channel);
   ssh_channel_free(channel);
   return SSH_OK;
+}
+
+int             forwarding_client(char* user, char* passwd, char* endpoint, int port)
+{
+  ssh_session   my_ssh_session;
+  int rc;
+
+  // Open session and set options
+  my_ssh_session = ssh_new();
+  if (my_ssh_session == NULL)
+    exit(-1);
+  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, endpoint);
+  ssh_options_set(my_ssh_session, SSH_OPTIONS_PORT, &port);
+
+  // Connect to server
+  rc = ssh_connect(my_ssh_session);
+  if (rc != SSH_OK)
+  {
+    fprintf(stderr, "Error connecting to localhost: %s\n",
+            ssh_get_error(my_ssh_session));
+    ssh_free(my_ssh_session);
+    exit(-1);
+  }
+
+  // Verify the server's identity
+  // For the source code of verify_knowhost(), check previous example
+  if (verify_knownhost(my_ssh_session) < 0)
+  {
+    ssh_disconnect(my_ssh_session);
+    ssh_free(my_ssh_session);
+    exit(-1);
+  }
+
+  // Authenticate ourselves
+  rc = ssh_userauth_password(my_ssh_session, user, passwd);
+
+  if (rc != SSH_AUTH_SUCCESS)
+  {
+    fprintf(stderr, "Error authenticating with password: %s\n",
+            ssh_get_error(my_ssh_session));
+    ssh_disconnect(my_ssh_session);
+    ssh_free(my_ssh_session);
+    exit(-1);
+  }
+
+  show_remote_processes(my_ssh_session);
+  ssh_disconnect(my_ssh_session);
+  ssh_free(my_ssh_session);
 }
--- src/ssh_proxy.h
+++ src/ssh_proxy.h
@@ -6,5 +6,7 @@
 #define SSHPROXY_SSH_PROXY_H
 
 int forwarding_client(char *user, char *passwd, char *endpoint, int port);
+int verify_knownhost(ssh_session session);
+int show_remote_processes(ssh_session session);
 
 #endif //SSHPROXY_SSH_PROXY_H
